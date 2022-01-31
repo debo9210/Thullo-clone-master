fed9304d457da86d87185e4dfa37fb678dca41a7
debo9210 <omololuadebowale1@gmail.com>
2022-01-31 04:34:11 -0500

    set up server and connect frontend to backend

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://debo9210:trelloClone@thullo-app.zwjcb.mongodb.net/Thullo-App?retryWrites=true&w=majority',
+  secretOrKey: 'thulloCloneApp',
+};
--- server.js
+++ server.js
@@ -1,11 +1,38 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const colors = require('colors')
-const passport = require('passport')
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const colors = require('colors');
+const passport = require('passport');
 
-const app = express()
+const app = express();
 
+//db congig
+const DB = require('./config/keys_dev').mongoURI;
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+  })
+  .then(() => console.log('Connected to database'.magenta))
+  .catch((err) => console.log(err));
 
-const port = process.ENV.PORT || 5000
+//body parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running @port ${port}`.rainbow);
+});
